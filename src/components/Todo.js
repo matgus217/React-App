f7f51a2a2eb14c80f26a60b140d9514501ea8958
matgus217 <>
2024-02-15 08:52:18 +0000

    Change className on fontawesomeicons

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -8,8 +8,8 @@ export const Todo = ({task, deleteTodo, editTodo, toggleComplete}) => {
     <div className="Todo">
         <p className={`${task.completed ? "completed" : "incompleted"}`} onClick={() => toggleComplete(task.id)}>{task.task}</p>
         <div>
-        <FontAwesomeIcon className="edit-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
-        <FontAwesomeIcon className="delete-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
+        <FontAwesomeIcon className="e-icon" icon={faPenToSquare} onClick={() => editTodo(task.id)} />
+        <FontAwesomeIcon className="d-icon" icon={faTrash} onClick={() => deleteTodo(task.id)} />
         </div>
     </div>
   )
